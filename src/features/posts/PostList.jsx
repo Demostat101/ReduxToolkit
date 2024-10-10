df2b3e9d5727b2c3cfa1e96466b7020bacafb7d2
Demostat101 <demostat1995@gmail.com>
2024-10-10 22:32:47 +0100

    add placeholder posts api

--- src/features/posts/PostList.jsx
+++ src/features/posts/PostList.jsx
@@ -1,6 +1,6 @@
 
 import { useSelector, useDispatch } from "react-redux";
-import { selectAllPosts, getPostsStatus, fetchPosts } from "./PostsSlice";
+import { selectAllPosts, getPostsStatus, fetchPosts, getPostsError } from "./PostsSlice";
 import { useEffect } from "react";
 import PostsExcerpt from "./PostsExcerpt";
 
@@ -8,7 +8,7 @@ const PostList = () => {
   const dispatch = useDispatch();
   const posts = useSelector(selectAllPosts);
   const postsStatus = useSelector(getPostsStatus);
-  
+  const error = useSelector(getPostsError);
 
   useEffect(() => {
     if (postsStatus === "idle") {
@@ -20,17 +20,19 @@ const PostList = () => {
     return <p>Loading...</p>;
   } else if (postsStatus === "succeeded") {
     return (
-      <section>
-        <h2>Posts</h2>
+      <section className="px-[30px] py-[20px] text-white">
+        <h2 className="font-[700] text-[30px]">Posts</h2>
         {posts.length > 0 ? (
-          posts.map((post) => <PostsExcerpt key={post.id} post={post} />)
+          posts.map((post) => <PostsExcerpt key={post.id} post={post} />).reverse()
         ) : (
           <p>No posts available.</p>
         )}
       </section>
     );
   } else if (postsStatus === "failed") {
-    return <p>Error fetching posts.</p>;
+    return <div className="w-full flex justify-center">
+      <p className="text-red-500 font-[600] text-[20px] text-center bg-white w-fit p-[10px]">{error}</p>
+    </div>;
   }
 
   return null; // Return null if none of the conditions are met
